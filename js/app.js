f691874ff109b56aa47a47034591016b55abc776
Amlexandria <antestumejorsonrisa@gmail.com>
2018-05-31 00:46:34 -0500

    Se agrega una función que convierte cualquier objeto a un array

--- js/app.js
+++ js/app.js
@@ -1,36 +1,93 @@
-console.log(Object.keys(data));
+//console.log(Object.keys(data));
 
-
-//ACCEDIENDO  A LAS SEDES
+//ACCEDIENDO  A LAS SEDES (devuelve un objeto)
 var arequipa = data.AQP;
 var cdmx = data.CDMX;
 var lima = data.LIM;
 var santiago = data.SCL;
-//console.log(arequipa);
+console.log("lo que contiene la sede arquipa (data.AQP)", arequipa);
 
-//ACCEDIENDO A LAS GENERACIONES
+//ACCEDIENDO A LAS GENERACIONES (devuelve las keys(strings) en un array)
 var aqpGenerations = Object.keys(arequipa);
 var cdmxGenerations = Object.keys(cdmx);
 var limaGenerations = Object.keys(lima);
 var stgoDeChileGenerations = Object.keys(santiago);
-console.log(aqpGenerations);
+console.log("lo que contiene las generaciones de arequipa (Object.keys(arequipa)",aqpGenerations);
+
+console.log(arequipa[aqpGenerations[0]]);
 
 
-var aqpGeneration2016 = arequipa["2016-2"];
-var aqpGeneration2017 = arequipa["2017-1"];
+//CREANDO FUNCION QUE CONVIERTA CUALQUIER OBJETO A UN ARRAY
+var objectToArray = function(object) {
+  var array2d = [];
+  var allThePropertiesOfTheObject = Object.keys(object);
 
-console.log(arequipa["2016-2"]);
+  for (var i = 0; i < allThePropertiesOfTheObject.length; i++) {
+        array2d.push(allThePropertiesOfTheObject[i], object[allThePropertiesOfTheObject[i]])
+  };
 
-//Obteniendo el total de estudiantes presentes por sede.
-//AREQUIPA
-
-var totalStudentsPerHeadquarters = function(aqpGenerations){
-  var sum = 0;
-  for(var i = 0; i < aqpGenerations.length; i++){
-      var students = aqpGenerations[i].students.length;
-      sum = students;
-      //var totalOfStudents = students+sum;
-      //sum++;
-  }
-  sum++;
+  return array2d;
 };
+
+
+console.log(objectToArray(arequipa));
+
+
+// //FORMANDO UN ARRAY CON LA INFO DE LAS SEDES para poder recorrerlo
+//
+// var arequipaArray = [
+//                       [aqpGenerations[0], arequipa[aqpGenerations[0]]],
+//                       [aqpGenerations[1], arequipa[aqpGenerations[1]]]
+//             ];
+//
+// console.log(arequipaArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// //Obteniendo el total de estudiantes presentes por sede.
+// //AREQUIPA
+//
+// var totalStudentsPerHeadquarters = function(sede){
+//   var sum = 0;
+//   for(var i = 0; i < aqpGenerations.length; i++){
+//       var students = arequipa[aqpGenerations[i]].students;
+//       var totalStudentsperGeneration = Object.keys(students).length;
+//       console.log(totalStudentsperGeneration);
+//
+//       sum += totalStudentsperGeneration;
+//   }
+//
+//   return console.log(sum);
+// };
+//
+// console.log(totalStudentsPerHeadquarters(arequipa));
+//
+//
+//
+//
+// /*var totalStudentsPerHeadquarters = function(generation){
+//   var sum = 0;
+//   for(var i = 0; i < aqpGenerations.length; i++){
+//       var students = aqpGenerations[i].students;
+//
+//       sum = students;
+//   }
+//   sum++;
+//
+//   return console.log(students);
+// };*/
