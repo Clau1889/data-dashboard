89721560b0bcc09e6ddc7b08d21adb73f27ac8ae
Amlexandria <antestumejorsonrisa@gmail.com>
2018-06-01 16:37:24 -0500

    Se agregan las funciones antes localizadas en app.js

--- js/second.js
+++ js/second.js
@@ -19,3 +19,49 @@ function getParameterByName(name, url) {
 
 
 
+
+///////////////////////////////////////////////////////////////////////////////
+
+//ACCEDIENDO  A LAS SEDES (devuelve un objeto)
+var arequipa = data.AQP;
+var cdmx = data.CDMX;
+var lima = data.LIM;
+var santiago = data.SCL;
+// console.log(arequipa);
+
+//ACCEDIENDO A LAS GENERACIONES (devuelve las keys(strings) en un array)
+var aqpGenerations = Object.keys(arequipa);
+var cdmxGenerations = Object.keys(cdmx);
+var limaGenerations = Object.keys(lima);
+var stgoDeChileGenerations = Object.keys(santiago);
+console.log(aqpGenerations);
+
+var aqpGeneration2016 = arequipa["2016-2"];
+var aqpGeneration2017 = arequipa["2017-1"];
+
+
+//CREANDO FUNCION QUE CONVIERTA CUALQUIER OBJETO A UN ARRAY
+var objectToArray = function(object) {
+  var array2d = [];
+  var allThePropertiesOfTheObject = Object.keys(object);
+
+  for (var i = 0; i < allThePropertiesOfTheObject.length; i++) {
+        array2d.push(allThePropertiesOfTheObject[i], object[allThePropertiesOfTheObject[i]])
+  };
+
+  return array2d;
+};
+
+
+//CONVIRTIENDO LOS OBJETOS DE LAS SEDES EN ARRAYS
+var arrayArequipa = objectToArray(arequipa);
+var arrayCdmx = objectToArray(cdmx);
+var arrayLima = objectToArray(lima);
+var arraySantiago = objectToArray(santiago);
+console.log(arrayArequipa);
+
+// CREANDO FUNCION QUE RETORNE UN ARRAY DE ESTUDIANTES DE CADA GENERACION
+
+// var arrayOfActiveStudentsperGeneration (arrayOfTheHeadquarters){
+//     for(var students)
+// }
